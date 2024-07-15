35002f9b9ede14d329da03504b2b7a778fdec9c0
SpearHyeok <painted4569@gmail.com>
2024-07-16 03:30:37 +0900

    Jest 테스트 추가

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders product card with correct details', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const productName = screen.getByText(/상품 이름/i);
+  const productDescription = screen.getByText(/상품 설명/i);
+  const productPrice = screen.getByText(/가격: 999원/i);
+
+  expect(productName).toBeInTheDocument();
+  expect(productDescription).toBeInTheDocument();
+  expect(productPrice).toBeInTheDocument();
+});
+
+test('alerts when product card is clicked', () => {
+  render(<App />);
+  
+  const productCard = screen.getByText(/상품 이름/i).closest('.prd_Card');
+
+  window.alert = jest.fn();
+  userEvent.click(productCard);
+
+  expect(window.alert).toHaveBeenCalledWith('상품 상세 페이지로 이동');
 });
